c528288480847cbb7b67dc19053b7124709dc5bd
julyperfect07 <abdallah.july05@gmail.com>
2025-11-01 14:16:58 +0300

    some changes

--- backend/src/models/user.modal.js
+++ backend/src/models/user.modal.js
@@ -4,7 +4,7 @@ const userSchema = new mongoose.Schema(
   {
     email: { type: String, required: true, unique: true },
     name: { type: String, required: true },
-    Image: { type: String, required: true },
+    image: { type: String, required: true },
     clerkId: { type: String, required: true, unique: true },
   },
   { timestamps: true }
